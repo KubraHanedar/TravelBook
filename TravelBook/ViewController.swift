881e9a71cc25a5d6821f574b2491c45dd7026651
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-08 17:37:59 +0300

    MapKit tanımlandı

---
+++ TravelBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelBook
+//
+//  Created by Kübra Hanedar on 8.01.2023.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
