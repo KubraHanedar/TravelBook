226f993f9c73331da1cf28a9e436b64bd5c8acab
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-09 17:08:10 +0300

    pinleme islemi icin gerekli fonksiyonlar eklendi

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -23,7 +23,25 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     
+    
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
         
+    }
+    
+    @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = "New Annotation"
+            annotation.subtitle = "Travel Book"
+            self.mapView.addAnnotation(annotation)
+        }
         
     }
 
