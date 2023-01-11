3f2339947cded50306b12fee2d71b387d3e55e2b
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-11 17:41:42 +0300

    addButton ile ekleme tamamlandı

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -15,19 +24,18 @@ class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
     }
-    */
+    
+
+   
 
 }
