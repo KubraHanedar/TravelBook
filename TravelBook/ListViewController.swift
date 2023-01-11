ebee13a616627a180cb01e5a5c8b9bc91ba96431
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-11 17:32:07 +0300

    ViewController ve tableView eklendi tanımlar yapıldı

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Kübra Hanedar on 11.01.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
